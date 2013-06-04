88ee017e1bac3a14436df8965d082a6941ab4dbf
Sean Cross <smcross@gmail.com>
2013-06-04 14:17:34 +0800

    nand: update to work with novena tapboard
    There are little things that are different, such as ALE and CLE being
    swapped.  Also, we assume that we don't have to remap pins anymore.

--- nand.cpp
+++ nand.cpp
@@ -3,9 +3,10 @@
 #include <stdint.h>
 #include "state.h"
 
+
 enum control_pins {
-    NAND_ALE = 2,
-    NAND_CLE = 1,
+    NAND_ALE = 1,
+    NAND_CLE = 2,
     NAND_WE = 4,
     NAND_RE = 8,
     NAND_CS = 16,
@@ -25,6 +26,7 @@ static uint8_t order[] = {
 };
 
 uint8_t nand_unscramble_byte(uint8_t byte) {
+    return byte;
 	return (
 		  ( (!!(byte&(1<<order[0]))) << 0)
 		| ( (!!(byte&(1<<order[1]))) << 1)
@@ -37,20 +39,6 @@ uint8_t nand_unscramble_byte(uint8_t byte) {
 	);
 }
 
-int nand_print(struct state *st, uint8_t data, uint8_t ctrl) {
-    Q_UNUSED(st);
-    fprintf(stderr,
-            "NAND %02x %c %c %c %c %c %c\n",
-            data,
-            ctrl&NAND_ALE?'A':' ',
-            ctrl&NAND_CLE?'C':' ',
-            ctrl&NAND_WE?'W':' ',
-            ctrl&NAND_RE?'R':' ',
-            ctrl&NAND_CS?'S':' ',
-            ctrl&NAND_RB?'B':' ');
-    return 0;
-}
-
 uint8_t nand_ale(uint8_t ctrl) {
     return ctrl&NAND_ALE;
 }
@@ -60,11 +48,11 @@ uint8_t nand_cle(uint8_t ctrl) {
 }
 
 uint8_t nand_we(uint8_t ctrl) {
-    return ctrl&NAND_WE;
+    return !(ctrl&NAND_WE);
 }
 
 uint8_t nand_re(uint8_t ctrl) {
-    return ctrl&NAND_RE;
+    return !(ctrl&NAND_RE);
 }
 
 uint8_t nand_cs(uint8_t ctrl) {
@@ -74,3 +62,18 @@ uint8_t nand_cs(uint8_t ctrl) {
 uint8_t nand_rb(uint8_t ctrl) {
     return ctrl&NAND_RB;
 }
+
+int nand_print(struct state *st, uint8_t data, uint8_t ctrl) {
+    Q_UNUSED(st);
+    fprintf(stderr,
+            "NAND %02x %c %c %c %c %c %c\n",
+            data,
+            nand_ale(ctrl)?'A':' ',
+            nand_cle(ctrl)?'C':' ',
+            nand_we(ctrl)?'W':' ',
+            nand_re(ctrl)?'R':' ',
+            nand_cs(ctrl)?'S':' ',
+            nand_rb(ctrl)?'B':' ');
+    return 0;
+}
+
